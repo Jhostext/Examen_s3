5b6a7955cf56a5d2ccb6a5eef2a41baaadc8620b
Jhostext <jhosep.lvega@gmail.com>
2021-01-14 13:25:55 -0500

    first commit

---
+++ but.js
@@ -0,0 +1,30 @@
+function presionamesa1(){ 
+    alert("presiono mesa 1");
+}
+function presionamesa2(){ 
+    alert("presiono mesa 2");
+}
+function presionamesa3(){ 
+    alert("presiono mesa 3");
+}
+function presionamesa4(){ 
+    alert("presiono mesa 4");
+}
+function presionamesa5(){ 
+    alert("presiono mesa 5");
+}
+function presionamesa6(){ 
+    alert("presiono mesa 6");
+}
+function presionamesa7(){ 
+    alert("presiono mesa 7");
+}
+function presionamesa8(){ 
+    alert("presiono mesa 8");
+}
+function presionamesa9(){ 
+    alert("presiono mesa 9");
+}
+function presionamesa10(){ 
+    alert("presiono mesa 10");
+}
---
+++ elm.js
@@ -0,0 +1,186 @@
+var autos =new Array();
+    var autos1=new Object();
+    autos1.Marca="Seat";
+    autos1.Color="Rojo";
+    autos1.Modelo="neon";
+    autos1.Matrícula="15554AA";
+    autos1.precio="10000€";
+    autos1.peso="2000Kg";
+
+    var autos2=new Object();
+    autos2.Marca="Ferrari";
+    autos2.Color="negro";
+    autos2.Modelo="Enzo";
+    autos2.Matrícula="12220BB";
+    autos2.precio="25000€";
+    autos2.peso="1500Kg";
+
+    var autos3=new Object();
+    autos3.Marca="Renault";
+    autos3.Color="Plomo";
+    autos3.Modelo="Clio";
+    autos3.Matrícula="98522SS";
+    autos3.precio="7000€";
+    autos3.peso="1000Kg";
+
+    var autos4=new Object();
+    autos4.Marca="Ford";
+    autos4.Color="crema";
+    autos4.Modelo="Shelby Gt500";
+    autos4.Matrícula="23610HH";
+    autos4.precio="9900€";
+    autos4.peso="1300Kg";
+
+    var autos5=new Object();
+    autos5.Marca="nissan";
+    autos5.Color="Rojo";
+    autos5.Modelo="Nismo";
+    autos5.Matrícula="89461NN";
+    autos5.precio="5800€";
+    autos5.peso="1100Kg";
+
+    var autos6=new Object();
+    autos6.Marca="Toyota";
+    autos6.Color="Blanco";
+    autos6.Modelo="Yaris";
+    autos6.Matrícula="55336KK";
+    autos6.precio="5500€";
+    autos6.peso="800Kg";
+
+    var autos7=new Object();
+    autos7.Marca="Mégane";
+    autos7.Color="Negro";
+    autos7.Modelo="Renaul II";
+    autos7.Matrícula="99925LL";
+    autos7.precio="11000€";
+    autos7.peso="1500Kg";
+
+    var autos8=new Object();
+    autos8.Marca="Tucson";
+    autos8.Color="Azul";
+    autos8.Modelo="Ginebra";
+    autos8.Matrícula="66332MF";
+    autos8.precio="15000€";
+    autos8.peso="1200Kg";
+
+    var autos9=new Object();
+    autos9.Marca="Ibiza";
+    autos9.Color="Gris";
+    autos9.Modelo="Seat";
+    autos9.Matrícula="11120KJ";
+    autos9.precio="7000€";
+    autos9.peso="1600Kg";
+
+    var autos10=new Object();
+    autos10.Marca="Astra";
+    autos10.Color="verde";
+    autos10.Modelo="Zafira";
+    autos10.Matrícula="88416BV";
+    autos10.precio="10900€";
+    autos10.peso="900Kg";
+
+    var autos11=new Object();
+    autos11.Marca="Dacia";
+    autos11.Color="Rojo";
+    autos11.Modelo="Duster";
+    autos11.Matrícula="74131AS";
+    autos11.precio="12000€";
+    autos11.peso="1300Kg";
+
+    var autos12=new Object();
+    autos12.Marca="Mazda";
+    autos12.Color="Rojo";
+    autos12.Modelo="CX-5";
+    autos12.Matrícula="88550RT";
+    autos12.precio="13000€";
+    autos12.peso="1500Kg";
+
+    var autos13=new Object();
+    autos13.Marca="Kia";
+    autos13.Color="Rojo";
+    autos13.Modelo="Soluto";
+    autos13.Matrícula="66612FF";
+    autos13.precio="12500€";
+    autos13.peso="1400Kg";
+
+    var autos14=new Object();
+    autos14.Marca="Volkswagen";
+    autos14.Color="negro";
+    autos14.Modelo="Suran";
+    autos14.Matrícula="66158GH";
+    autos14.precio="18000€";
+    autos14.peso="1599Kg";
+
+    var autos15=new Object();
+    autos15.Marca="Suzuki";
+    autos15.Color="Plomo";
+    autos15.Modelo="Ignis";
+    autos15.Matrícula="98456ÑÑ";
+    autos15.precio="17000€";
+    autos15.peso="1800Kg";
+
+    var autos16=new Object();
+    autos16.Marca="Hyundai";
+    autos16.Color="verde";
+    autos16.Modelo="Elantra";
+    autos16.Matrícula="66765PL";
+    autos16.precio="16000€";
+    autos16.peso="1500Kg";
+
+    var autos17=new Object();
+    autos17.Marca="Ford";
+    autos17.Color="Rojo";
+    autos17.Modelo="F-150";
+    autos17.Matrícula="95362JK";
+    autos17.precio="12000€";
+    autos17.peso="1600Kg";
+
+    var autos18=new Object();
+    autos18.Marca="Toyota";
+    autos18.Color="Rojo";
+    autos18.Modelo="Sequoia";
+    autos18.Matrícula="88885HH";
+    autos18.precio="13000€";
+    autos18.peso="1800Kg";
+
+    var autos19=new Object();
+    autos19.Marca="Lincoln";
+    autos19.Color="Rojo";
+    autos19.Modelo="Navigator";
+    autos19.Matrícula="66602GG";
+    autos19.precio="18000€";
+    autos19.peso="1800Kg";
+
+    var autos20=new Object();
+    autos20.Marca="Chevrolet";
+    autos20.Color="Rojo";
+    autos20.Modelo="Silverado 1500";
+    autos20.Matrícula="98551BT";
+    autos20.precio="15000€";
+    autos20.peso="1600Kg";
+
+    autos.push (autos1);
+    autos.push (autos2);
+    autos.push (autos3);
+    autos.push (autos4);
+    autos.push (autos5);
+    autos.push (autos6);
+    autos.push (autos7);
+    autos.push (autos8);
+    autos.push (autos9);
+    autos.push (autos10);
+    autos.push (autos11);
+    autos.push (autos12);
+    autos.push (autos13);
+    autos.push (autos14);
+    autos.push (autos15);
+    autos.push (autos16);
+    autos.push (autos17);
+    autos.push (autos18);
+    autos.push (autos19);
+    autos.push (autos20);
+
+    function mostrar(){
+        alert(autos[1].Modelo);
+    }
+
